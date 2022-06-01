5e6827eb63a4f920a33385f6193fb35ab968d2a6
John Philip Calugcugan <jcalugcugan@webcreek.com>
2022-06-01 21:09:05 +0800

    Initial Commit

---
+++ Persistent Bugger/PersistentBugger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Persistent_Bugger
+{
+    public class PersistentBugger
+    {
+        public int Persistence(long n)
+        {
+            var persistenceCtr = 0;
+            var inputMod = n.ToString();
+
+            while (inputMod.Length != 1)
+            {
+                var wkInput = 1;
+                for (int strIdx = 0; strIdx < inputMod.Length; strIdx++)
+                {
+                    wkInput *= Convert.ToInt32(inputMod.Substring(strIdx, 1));
+
+                    if ((strIdx + 1) == inputMod.Length)
+                    {
+                        inputMod = wkInput.ToString();
+                        persistenceCtr += 1;
+                        break;
+                    }
+                }
+            }
+
+            return persistenceCtr;
+        }
+    }
+}
---
+++ Persistent Bugger/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Persistent_Bugger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                *  Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+                *  For example (Input --> Output):
+                    39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+                    999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+                    4 --> 0 (because 4 is already a one-digit number)
+            */
+
+            var persistentBugger = new PersistentBugger();
+            int answer1 = persistentBugger.Persistence(39);
+            int answer2 = persistentBugger.Persistence(999);
+            int answer3 = persistentBugger.Persistence(4);
+
+            Console.ReadLine(); // Wait for press key before exit
+        }
+    }
+}
